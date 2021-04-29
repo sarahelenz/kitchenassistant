07544e975207f1c5b0473f4f10a573a3cb1d0135
Sarah Elenz <selenz3112@stu.d214.org>
2021-04-29 15:38:08 -0400

    Initial Commit

---
+++ Kitchen Assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kitchen Assistant
+//
+//  Created by user178354 on 4/29/21.
+//  Copyright © 2021 user178354. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
