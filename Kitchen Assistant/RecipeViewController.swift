8fd48b3d17b438ed9842ee5f9101b22f732450f3
Sarah Elenz <selenz3112@stu.d214.org>
2021-05-03 15:18:43 -0500

    accessed the title of the food chosen

--- Kitchen Assistant/RecipeViewController.swift
+++ Kitchen Assistant/RecipeViewController.swift
@@ -8,15 +8,59 @@
 
 import UIKit
 
-class RecipeViewController: UIViewController {
-
+class RecipeViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var recipeTableView: UITableView!
+    var recipeDetails: [String] = []
+    var recipeInstructions: [String] = []
+    var randomBreakfast: String = ""
+    var randomLunch: String = ""
+    var randomDinner: String = ""
+    var randomDessert: String = ""
+    var randomSnack: String = ""
+    var breakfastNames: [Any] = []
+    var lunchNames: [Any] = []
+    var dinnerNames: [Any] = []
+    var dessertNames: [Any] = []
+    var snackNames: [Any] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        assignRecipetoName()
+        DispatchQueue.main.async {
+            self.recipeTableView.reloadData()
+        }
+     //   print(recipeInstructions)
         
     }
     
-
-  
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipeInstructions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        DispatchQueue.main.async {
+            self.recipeTableView.reloadData()
+        }
+        let cell2 = recipeTableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath)
+        cell2.textLabel?.text = "\(recipeInstructions[indexPath.row])"
+        return cell2
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc2 = segue.destination as! SecondViewController
+        let nvc1 = segue.destination as! ViewController
+        nvc1.breakfastNames = breakfastNames
+        nvc1.lunchNames = lunchNames
+        nvc1.dinnerNames = dinnerNames
+        nvc1.dessertNames = dessertNames
+        nvc1.snackNames = snackNames
+    }
+    
+    func assignRecipetoName(){
+        
+        }
+        
+    
+    
 }
--- Kitchen Assistant/ViewController.swift
+++ Kitchen Assistant/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    
     var both: [Any] = []
     var mealNames: [String] = []
     var foodName: [Any] = []
     var foodCount:[Any] = []
+    var recipeDetails: [Any] = []
     var breakfastNames: [Any] = []
     var lunchNames: [Any] = []
     var dinnerNames: [Any] = []
@@ -25,6 +26,8 @@ class ViewController: UIViewController {
     var dinnerCount: Int = 0
     var dessertCount: Int = 0
     var snackCount: Int = 0
+    var chosenRecipe = ""
+
     
     @IBOutlet weak var yourRecipeButton: UIButton!
     
@@ -35,9 +38,10 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         getFirebaseData()
         super.viewDidLoad()
-       
+        
     }
     
+   
     public func getFirebaseData(){
         var ref = Database.database().reference()
         ref.observeSingleEvent(of: .value) { (snapshot) in
@@ -46,45 +50,46 @@ class ViewController: UIViewController {
                 let meal = data.key
                 let recipeAndDetails = data.value as! NSDictionary
                 let recipeName = recipeAndDetails.allKeys
+                let recipeInstructions = recipeAndDetails.allValues
+                
+                
                 
                 self.mealNames.append(meal)
                 self.foodName.append(contentsOf: recipeName)
                 self.foodCount.append(recipeName)
-               
+                self.recipeDetails.append(recipeInstructions)
+                
                 
                 for meal in self.mealNames{
-                                if meal == "Breakfast"{
-                                    self.breakfastCount = (self.foodCount[0] as AnyObject).count
-                                    self.breakfastNames.append(contentsOf: self.foodName[0..<self.breakfastCount])
-                                }else if meal == "Lunch"{
-                                    self.lunchCount = (self.foodCount[3] as AnyObject).count + self.dinnerCount
-                                    self.lunchNames.append(contentsOf: self.foodName[self.dinnerCount..<self.lunchCount])
-                                    print(self.foodCount[3])
-                                }else if meal == "Dinner"{
-                                    self.dinnerCount = (self.foodCount[2] as AnyObject).count + self.dessertCount
-                                    self.dinnerNames.append(contentsOf: self.foodName[self.dessertCount..<self.dinnerCount])
-                                }else if meal == "Dessert"{
-                                    self.dessertCount = (self.foodCount[1] as AnyObject).count + self.breakfastCount
-                                    self.dessertNames.append(contentsOf: self.foodName[self.breakfastCount..<self.dessertCount])
-                                }else if meal == "Snack"{
-                                    self.snackCount = (self.foodCount[4] as AnyObject).count + self.lunchCount
-                                    self.snackNames.append(contentsOf: self.foodName[self.lunchCount..<self.snackCount])
-                                    
+                    if meal == "Breakfast"{
+                        self.breakfastCount = (self.foodCount[0] as AnyObject).count
+                        self.breakfastNames.append(contentsOf:
+                            self.foodName[0..<self.breakfastCount])
+                    }else if meal == "Lunch"{
+                        self.lunchCount = (self.foodCount[3] as AnyObject).count + self.dinnerCount
+                        self.lunchNames.append(contentsOf: self.foodName[self.dinnerCount..<self.lunchCount])
+                        print(self.foodCount[3])
+                    }else if meal == "Dinner"{
+                        self.dinnerCount = (self.foodCount[2] as AnyObject).count + self.dessertCount
+                        self.dinnerNames.append(contentsOf:
+                        self.foodName[self.dessertCount..<self.dinnerCount])
+                    }else if meal == "Dessert"{
+                        self.dessertCount = (self.foodCount[1] as AnyObject).count + self.breakfastCount
+                        self.dessertNames.append(contentsOf: self.foodName[self.breakfastCount..<self.dessertCount])
+                    }else if meal == "Snack"{
+                        self.snackCount = (self.foodCount[4] as AnyObject).count + self.lunchCount
+                        self.snackNames.append(contentsOf: self.foodName[self.lunchCount..<self.snackCount])
+                        
                     }
-                            }
-              //  print(self.foodName)
-             //   print(self.foodName[0...1])
-             //   print(recipeAndDetails)
-              //  print(self.lunchNames)
-                print(self.breakfastCount)
-                print(self.lunchNames)
+                }
+                print(self.recipeDetails)
                 
             }
         }
         
     }
     
-      
+    
     
     
     
@@ -109,12 +114,16 @@ class ViewController: UIViewController {
         yourRecipeButton.setTitle("\(randomLunch)", for: .normal)
     }
     @IBAction func whenBreakfastRecipeButtonPressed(_ sender: Any) {
-            getFirebaseData()
+        getFirebaseData()
         
-      //  print(breakfastNames)
+        //  print(breakfastNames)
         var random = breakfastNames.randomElement()!
-        yourRecipeButton.setTitle("\(random)", for: .normal)
+         yourRecipeButton.setTitle("\(random)", for: .normal)
     }
-
+    
+    @IBAction func whenRecipeButtonPressed(_ sender: Any) {
+        chosenRecipe = yourRecipeButton.currentTitle!
+        print(chosenRecipe)
+    }
 }
 
